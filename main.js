6383d51d4c0d831c17ed1afc644037d297c9fb35
noah-not-tj <noah.h.pickett@gmail.com>
2026-03-29 19:04:04 -0500

    current version - motor is next

--- main.js
+++ main.js
@@ -1,53 +1,58 @@
-  const canvas = document.createElement("canvas");
-  document.body.appendChild(canvas);
-  canvas.width = 1024;
-  canvas.height = 1024;
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+canvas.width = 1024;
+canvas.height = 1024;
 
-  canvas.style.width = '250px';
-  canvas.style.height = '250px';
-  const ctx = canvas.getContext("2d");
+canvas.style.width = '250px';
+canvas.style.height = '250px';
+const ctx = canvas.getContext("2d");
 
-  ctx.translate(0, canvas.height);
-  ctx.scale(1, -1);
+ctx.translate(0, canvas.height-100);
+ctx.scale(1, -1);
 
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 10;
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 20;
 
-  window.onerror = (e, l, s, c, r) => alert(r.stack);
 
-  let position = [10,10]; 
-  let input = document.getElementById("input");
-  input.addEventListener('keydown', async (event) => {
-    if (event.key === 'Enter') {
-      await fetchanddraw(input.value);
-      input.value = '';
-    }
-  });
-  
- 
-async function fetchanddraw (character) {
-  ctx.clearRect(0,0, canvas.width, canvas.height);
+window.onerror = (e, l, s, c, r) => alert(r.stack);
+
+let position = [10, 10];
+let input = document.getElementById("input");
+input.focus();
+input.addEventListener('keydown', async (event) => {
+  if (event.key === 'Enter') {
+    await fetchanddraw(input.value);
+    input.value = '';
+  }
+});
+
+
+async function fetchanddraw(character) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   fetch(`https://raw.githubusercontent.com/chanind/hanzi-writer-data/refs/heads/master/data/${character}.json`).then((response) => response.text()).then(async (text) => {
     //alert(text);
-    const sleep = (ms) => new Promise((resolve)=>setTimeout(resolve, ms)); 
+    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     json = JSON.parse(text);
     for (let stroke of json.medians) {
-        position = [...stroke[0]];
-        for (let coord of stroke) {
-            drawline(coord[0], coord[1]);
-            await sleep(100); 
-        }
+      position = [...stroke[0]];
+      //sendToArduino(`M${position[0]}, ${position[1]}`);
+      await sleep(500);
+      for (let coord of stroke) {
+        drawline(coord[0], coord[1]);
+        await sleep(100);
+      }
     }
 
   });
 }
 
-  function drawline(x, y) {
-      ctx.beginPath();
-      ctx.moveTo(position[0], position[1]);
-      ctx.lineTo(x, y);
-      ctx.stroke();
-      ctx.closePath();
-      position[0] = x;
-      position[1] = y;
-  }
+function drawline(x, y) {
+  ctx.beginPath();
+  ctx.moveTo(position[0], position[1]);
+  ctx.lineTo(x, y); 
+  //sendToArduino(`L${x}, ${y}`);
+  ctx.stroke();
+  ctx.closePath();
+  position[0] = x;
+  position[1] = y;
+}
